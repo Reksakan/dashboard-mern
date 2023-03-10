185b04f8d2ca8e2a2766bc01ee93f004f087ae20
reksakan <reksakan@reksa-debian.debian>
2023-03-10 00:22:38 -0500

    Products component is updated

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -28,8 +28,50 @@ const Product = ({
 
   return (
     <Card
-      //WORKING PROGRESS
-    ></Card>
+      sx={{
+        backgroundImage: "none",
+        backgroundColor: theme.palette.background.alt,
+        borderRadius: "0.55rem",
+      }}
+    >
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color={theme.paletter.secondary[700]} gutterBottom>
+          {category}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography sx={{ mb: "1.5rem "}} color={theme.palette.secondary[400]}>
+          ${Number(price).toFixed(2)}
+        </Typography>
+        <Rating value={rating} readOnly />
+        <Typography variant="body2">{description}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant="primary"
+          size="small"
+          onClick={()=> setIsExpanded(!isExpanded)}
+        >
+          See More
+        </Button>
+      </CardActions>
+      <Collapse
+        in={isExpanded}
+        timeout="auto"
+        unmountOnExit
+        sx={{
+          color: theme.palette.neutral[300]
+        }}
+      >
+        <CardContent>
+          <Typography>id: {_id}</Typography>
+          <Typography>Supply left: {supply}</Typography>
+          <Typography>Yearly Sales This Year: {stat.yearlySalesTotal}</Typography>
+          <Typography>Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
   )
 }
 
@@ -49,10 +91,32 @@ const Products = () => {
           justifyContent="space-between"
           columnGap="1.33%"
           sx={{
-            "& > div" : { gridColumn: isNonMobile ? underfined : "span 4"}
+            "& > div" : { gridColumn: isNonMobile ? undefined : "span 4"}
           }}
         >
-          {data.map(()=> {})}
+          {data.map(
+            ({
+              _id,
+              name,
+              description,
+              price,
+              rating,
+              category,
+              supply,
+              stat,
+            })=> (
+              <Product 
+              key={_id}
+              _id={_id}
+              name={name}
+              description={description}
+              price={price}
+              rating={rating}
+              category={category}
+              supply={supply}
+              stat
+              />
+            ))}
         </Box>) : (
           <>Loading...</>
         )}
