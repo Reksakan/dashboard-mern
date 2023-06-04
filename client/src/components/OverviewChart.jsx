7dda3eb84918ab60b2e0912a821d4a1f848b130c
reksakan <reksakan@reksa-debian.debian>
2023-06-04 04:54:09 -0400

    Sales doesn't realized. Issue with data fetching

---
+++ client/src/components/OverviewChart.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
+import { useTheme } from '@mui/material';
+import { useGetSalesQuery } from 'state/api';
+
+const OverviewChart = ({isDashboard = false, view }) => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetSalesQuery();
+  console.log('data: ', data);
+  console.log('isLoading: ', isLoading);
+
+  return (
+    <div>OverviewChart</div>
+  )
+}
+
+export default OverviewChart;
--- server/index.js
+++ server/index.js
@@ -18,7 +18,14 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from './models/Transaction.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
+import OverallStat from "./models/OverallStat.js";
+import { 
+    dataUser, 
+    dataProduct, 
+    dataProductStat, 
+    dataTransaction,
+    dataOverallStat,
+ } from "./data/index.js";
 
 /* CONFIGURATIONS */
 dotenv.config()
@@ -53,7 +60,7 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(()=> {
     app.listen(PORT, ()=> console.log(`Server port" ${PORT}`));
-    
+    // OverallStat.insertMany(dataOverallStat);
     /* ONLY ADD DATA ONE TIME*/
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
