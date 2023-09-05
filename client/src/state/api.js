344b3d777a5a239ce956c3e32475d17c11cb578e
reksakan <reksakan@reksa-debian.debian>
2023-09-05 00:56:01 -0400

    Dashboard UI is completed

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -11,7 +11,8 @@ export const api = createApi({
   "Geography", 
   "Sales", 
   "Admins", 
-  "Performance"
+  "Performance",
+  "Dashboard"
 ],
   endpoints: (build) => ({
     getUser: build.query({
@@ -49,7 +50,11 @@ export const api = createApi({
     getUserPerformance: build.query({
       query: (id) => `management/performance/${id}`,
       providesTags: ["Performance"]
-    })
+    }),
+    getDashboard: build.query({
+      query: () => 'general/dashboard',
+      providesTags: ["Dashboard"]
+    })  
   })
 });
 
@@ -62,4 +67,5 @@ export const {
   useGetSalesQuery,
   useGetAdminsQuery,
   useGetUserPerformanceQuery,
+  useGetDashboardQuery,
 } = api;
